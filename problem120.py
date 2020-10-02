e85ec175663ea6806f3409edf6422e1cc5c1b397
Julian van Doorn <jvdoorn@antarc.com>
2020-10-02 10:56:19 +0200

    Added problem 120.

---
+++ problem120.py
@@ -0,0 +1,17 @@
+"""
+Graph for problem 120 of SP1.
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+x = np.linspace(0, 20, 1000)
+y = np.square(x) * np.exp(x) / (1 + np.exp(x))**2
+
+plt.style.use(['science', 'grid'])
+plt.plot(x, y)
+plt.title('Schottky heat capacity')
+plt.xlabel('$\\frac{\\epsilon}{k_BT}$')
+plt.ylabel('$\\frac{C_V}{Nk_B}$')
+plt.savefig('/Users/julian/OneDrive - Universiteit Leiden/SP1/svg/problem120.svg')
+plt.show()
